13e0219411d77724ac460f611e3f568e3ceffea6
Pierre Lamarche <lamarche.p@gmail.com>
2022-12-03 16:25:02 +0100

    day 01 - plus simple

--- day01/day01.py
+++ day01/day01.py
@@ -16,3 +16,14 @@ max(somme)
 
 somme.sort()
 sum(somme[-3:])
+
+## plus rapide
+
+with open(fichier, mode='r') as f:
+    puzzle = f.read().split('\n\n')
+
+somme = [sum([int(c) for c in case.split('\n') if c != '']) for case in puzzle]
+max(somme)
+
+somme.sort()
+sum(somme[-3:])
